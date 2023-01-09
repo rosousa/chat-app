c6adb928be49892aad413b214e21ec83cdf3e750
rosousa <rodolfozsousa@gmail.com>
2023-01-08 21:21:02 -0300

    add register layout

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles/reset.scss";
+import Register from "./pages/Register/Register"
+
+function App() {
+  return (
+    <Register />
+  )
+}
+
+export default App
---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Login: React.FC = () => {
+    return (
+        <p>Login</p>
+    )
+}
+
+export default Login
